3920cc4863c0a3400d8748807944fbcf065ef737
diliani <132509989+diliani@users.noreply.github.com>
2023-11-11 10:57:56 -0300

    d

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 document.querySelector ('.tecla_clap').onclick= tocaSomClap
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector ('.tecla_tim').onclick= tocaSomTim
